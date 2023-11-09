2efb0918d4d49922379246bcf9874fd7e116daa3
Ashutosh <ashutosh1412003@gmail.com>
2023-11-09 20:32:18 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSubmit = async(e) => {
   const messageDiv = document.getElementById(uniqueID);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://openai-cpe6.onrender.com/',{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
